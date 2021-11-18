abc911a9ccbc81be70056d3c3f5d6086c156e627
Karel Gudera <karel.gudera@datasentics.com>
2021-11-18 19:58:42 +0100

    DI container is now created in bootstrap

--- src/__myproject__/app/bootstrap.py
+++ src/__myproject__/app/bootstrap.py
@@ -31,3 +31,11 @@ if "APP_ENV" not in os.environ:
 
 if "DAIPE_BOOTSTRAPPED" not in os.environ:
     os.environ["DAIPE_BOOTSTRAPPED"] = "1"
+
+# COMMAND ----------
+
+from daipecore.decorator.ContainerManager import ContainerManager  # noqa
+from daipecore.bootstrap.container_factory import create_container  # noqa
+
+container = create_container(os.environ["APP_ENV"])
+ContainerManager.set_container(container)
