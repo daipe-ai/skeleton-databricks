05f77b9b3a9a38a35f9586f6b23bbbac3c7e9e22
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2021-12-14 16:11:50 +0100

    Change bootstrap structure

---
+++ bootstrap/bootstrap_base.py
@@ -0,0 +1,32 @@
+# Databricks notebook source
+# MAGIC %run ./install_benvy
+
+# COMMAND ----------
+
+from benvy.databricks.repos import bootstrap
+from benvy.databricks.detector import is_databricks_repo
+
+if is_databricks_repo():
+    bootstrap.install()
+
+# COMMAND ----------
+
+from benvy.databricks.repos import bootstrap
+from benvy.databricks.detector import is_databricks_repo
+
+if is_databricks_repo():
+    bootstrap.setup_env()
+
+# COMMAND ----------
+
+import os
+import colorlog
+
+if "APP_ENV" not in os.environ:
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter('%(log_color)s%(levelname)s: %(message)s'))
+    logger = colorlog.getLogger('example')
+    logger.addHandler(handler)
+    
+    logger.warning("Using default DEV environment. Please set the APP_ENV variable on cluster and delete this cell.")
+    os.environ["APP_ENV"] = "dev"
---
+++ bootstrap/install_benvy.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# MAGIC %sh
+# MAGIC pip install benvy==1.2.2
