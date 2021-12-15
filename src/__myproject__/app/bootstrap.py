d1d2039bfa4b7c34393050e844a918d45660b1f7
kutny <jirkakoutny@gmail.com>
2021-12-15 15:25:53 +0100

    container refresher added
    For the developers to be able to use updated config parameters in notebook without clearing notebook state
    
    See https://github.com/daipe-ai/daipe-core/pull/14

--- src/__myproject__/app/bootstrap.py
+++ src/__myproject__/app/bootstrap.py
@@ -22,5 +22,17 @@ if os.environ["APP_ENV"] == "dev":
 from daipecore.decorator.ContainerManager import ContainerManager
 from daipecore.bootstrap.container_factory import create_container
 
-container = create_container(os.environ["APP_ENV"])
-ContainerManager.set_container(container)
+def init_container():
+    ContainerManager.set_container(create_container(os.environ["APP_ENV"]))
+
+init_container()
+
+# COMMAND ----------
+
+from databricksbundle.notebook.helpers import get_notebook_path
+from daipecore.container import container_refresher
+
+if os.environ["APP_ENV"] == "dev" and "watcher_thread" not in globals():
+    configs_dir = "/Workspace" + "/".join(get_notebook_path().split("/")[:6]) + "/_config"
+    watcher_logger, watcher_thread = container_refresher.watch_configs(configs_dir, init_container)
+    watcher_logger.print()
