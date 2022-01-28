06df8c49d845d3a63a1e6bdb93c6d2d9db3a7749
kutny <jirkakoutny@gmail.com>
2022-01-28 17:35:01 +0100

    All dependencies can be installed from local path
    By setting the DAIPE_DEPENDENCIES_DIR env var.
    To support Databricks deployments without internet access.

--- bootstrap/install_benvy.py
+++ bootstrap/install_benvy.py
@@ -1,3 +1,15 @@
 # Databricks notebook source
-# MAGIC %sh
-# MAGIC pip install benvy==1.3.1
+import os
+import IPython
+
+PENVY_VERSION = "1.2.3"
+BENVY_VERSION = "1.3.2"
+
+command = f"pip install penvy=={PENVY_VERSION} benvy=={BENVY_VERSION}"
+
+if "DAIPE_DEPENDENCIES_DIR" in os.environ:
+    command += f" --no-index --find-links {os.environ['DAIPE_DEPENDENCIES_DIR']}"
+
+output_lines = IPython.get_ipython().run_line_magic("system", command)
+
+print("\n".join(output_lines))
