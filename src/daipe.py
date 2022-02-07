1d6315d36e56a4ba9bb5ec95f3eb952cffb767c2
kutny <jirkakoutny@gmail.com>
2022-02-07 15:54:41 +0100

    # noqa removed

--- src/daipe.py
+++ src/daipe.py
@@ -5,38 +5,38 @@
 # pylint: disable=unused-import, reimported
 
 # imports from daipecore
-from daipecore.decorator.notebook_function import notebook_function  # noqa: F401
-from daipecore.widgets.Widgets import Widgets  # noqa: F401
-from daipecore.widgets.get_widget_value import get_widget_value  # noqa: F401
+from daipecore.decorator.notebook_function import notebook_function
+from daipecore.widgets.Widgets import Widgets
+from daipecore.widgets.get_widget_value import get_widget_value
 
 # imports from datalakebundle
-from daipecore.decorator.notebook_function import notebook_function  # noqa: F401
-from datalakebundle.notebook.decorator.transformation import transformation  # noqa: F401
-from datalakebundle.notebook.decorator.data_frame_loader import data_frame_loader  # noqa: F401
-from datalakebundle.notebook.decorator.data_frame_saver import data_frame_saver  # noqa: F401
-from pysparkbundle.csv.csv_reader import read_csv  # noqa: F401
-from pysparkbundle.csv.csv_append import csv_append  # noqa: F401
-from pysparkbundle.csv.csv_overwrite import csv_overwrite  # noqa: F401
-from pysparkbundle.csv.csv_write_ignore import csv_write_ignore  # noqa: F401
-from pysparkbundle.csv.csv_write_errorifexists import csv_write_errorifexists  # noqa: F401
-from pysparkbundle.delta.delta_reader import read_delta  # noqa: F401
-from pysparkbundle.delta.delta_append import delta_append  # noqa: F401
-from pysparkbundle.delta.delta_overwrite import delta_overwrite  # noqa: F401
-from pysparkbundle.delta.delta_write_ignore import delta_write_ignore  # noqa: F401
-from pysparkbundle.delta.delta_write_errorifexists import delta_write_errorifexists  # noqa: F401
-from pysparkbundle.json.json_reader import read_json  # noqa: F401
-from pysparkbundle.json.json_append import json_append  # noqa: F401
-from pysparkbundle.json.json_overwrite import json_overwrite  # noqa: F401
-from pysparkbundle.json.json_write_ignore import json_write_ignore  # noqa: F401
-from pysparkbundle.json.json_write_errorifexists import json_write_errorifexists  # noqa: F401
-from pysparkbundle.parquet.parquet_reader import read_parquet  # noqa: F401
-from pysparkbundle.parquet.parquet_append import parquet_append  # noqa: F401
-from pysparkbundle.parquet.parquet_overwrite import parquet_overwrite  # noqa: F401
-from pysparkbundle.parquet.parquet_write_ignore import parquet_write_ignore  # noqa: F401
-from pysparkbundle.parquet.parquet_write_errorifexists import parquet_write_errorifexists  # noqa: F401
-from datalakebundle.table.parameters.table_params import table_params  # noqa: F401
-from datalakebundle.table.write.table_append import table_append  # noqa: F401
-from datalakebundle.table.write.table_overwrite import table_overwrite  # noqa: F401
-from datalakebundle.table.write.table_upsert import table_upsert  # noqa: F401
-from datalakebundle.table.read.table_reader import read_table  # noqa: F401
-from datalakebundle.table.schema.TableSchema import TableSchema  # noqa: F401
+from daipecore.decorator.notebook_function import notebook_function
+from datalakebundle.notebook.decorator.transformation import transformation
+from datalakebundle.notebook.decorator.data_frame_loader import data_frame_loader
+from datalakebundle.notebook.decorator.data_frame_saver import data_frame_saver
+from pysparkbundle.csv.csv_reader import read_csv
+from pysparkbundle.csv.csv_append import csv_append
+from pysparkbundle.csv.csv_overwrite import csv_overwrite
+from pysparkbundle.csv.csv_write_ignore import csv_write_ignore
+from pysparkbundle.csv.csv_write_errorifexists import csv_write_errorifexists
+from pysparkbundle.delta.delta_reader import read_delta
+from pysparkbundle.delta.delta_append import delta_append
+from pysparkbundle.delta.delta_overwrite import delta_overwrite
+from pysparkbundle.delta.delta_write_ignore import delta_write_ignore
+from pysparkbundle.delta.delta_write_errorifexists import delta_write_errorifexists
+from pysparkbundle.json.json_reader import read_json
+from pysparkbundle.json.json_append import json_append
+from pysparkbundle.json.json_overwrite import json_overwrite
+from pysparkbundle.json.json_write_ignore import json_write_ignore
+from pysparkbundle.json.json_write_errorifexists import json_write_errorifexists
+from pysparkbundle.parquet.parquet_reader import read_parquet
+from pysparkbundle.parquet.parquet_append import parquet_append
+from pysparkbundle.parquet.parquet_overwrite import parquet_overwrite
+from pysparkbundle.parquet.parquet_write_ignore import parquet_write_ignore
+from pysparkbundle.parquet.parquet_write_errorifexists import parquet_write_errorifexists
+from datalakebundle.table.parameters.table_params import table_params
+from datalakebundle.table.write.table_append import table_append
+from datalakebundle.table.write.table_overwrite import table_overwrite
+from datalakebundle.table.write.table_upsert import table_upsert
+from datalakebundle.table.read.table_reader import read_table
+from datalakebundle.table.schema.TableSchema import TableSchema
